13ef280dd473d2ed25b5a6c96378626d299ca399
Louis Rabiet <louis.rabiet@gmail.com>
2017-11-08 08:59:05 +0100

    Create startup-heroku.sh

---
+++ startup-heroku.sh
@@ -0,0 +1,24 @@
+# point to the correct configuration and webapp
+CATALINA_BASE=`pwd`/site/target/cargo/configurations/tomcat7x
+export CATALINA_BASE
+
+#move the admin application to the tomcat deploy
+#mv ./admin/target/admin.war $CATALINA_BASE/webapps
+
+#remove the extraneous webapps that we don't need
+rm -rf $CATALINA_BASE/webapps/cargocpc*
+rm -rf $CATALINA_BASE/webapps/host-manager
+rm -rf $CATALINA_BASE/webapps/manager
+
+#copy over the Heroku config files
+cp ./site/heroku-server.xml $CATALINA_BASE/conf/server.xml
+
+#make the Tomcat scripts executable
+chmod a+x ./site/target/cargo/installs/apache-tomcat-7.0.29/apache-tomcat-7.0.29/bin/*.sh
+
+#set the correct port and database settings
+JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=768M -Xmx768M -Dhttp.port=$PORT -DDATABASE_URL=$DATABASE_URL"
+export JAVA_OPTS
+
+#start Tomcat
+./site/target/cargo/installs/apache-tomcat-7.0.29/apache-tomcat-7.0.29/bin/catalina.sh run
